f0e6352631255e86cc8b263630a914b38e2fcce1
AndrewFernando <72968022@continental.edu.pe>
2023-04-12 17:17:34 -0500

    v.5

--- Operaciones.py
+++ Operaciones.py
@@ -20,6 +20,29 @@ while True:
         while(opcion < 1 or opcion > 5):
             print("Dato Inválido, vuelve a ingresar la opción")
             opcion = int(input("Ingrese una opción válida: "))
+        if (opcion == 1):
+            a = float(input("Ingrese el primer número: "))
+            b = float(input("Ingrese el segundo número: "))
+            resultado = suma(a, b)
+            print("Resultado: ", resultado)
+        if (opcion == 2):
+            a = float(input("Ingrese el primer número: "))
+            b = float(input("Ingrese el segundo número: "))
+            resultado = resta(a, b)
+            print("Resultado: ", resultado)
+        if (opcion == 3):
+            a = float(input("Ingrese el primer número: "))
+            b = float(input("Ingrese el segundo número: "))
+            resultado = multiplicacion(a, b)
+            print("Resultado: ", resultado)
+        if (opcion == 4):
+            a = float(input("Ingrese el primer número: "))
+            b = float(input("Ingrese el segundo número: "))
+            resultado = division(a, b)
+            print("Resultado: ", resultado)
+        if (opcion == 5):
+            print("Gracias por usar la aplicación")
+            break
     except ValueError as VE:
         print("\n !!!!!Ingrese Dato Válido \n")
     except Exception as e:
