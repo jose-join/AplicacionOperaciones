d819b17f30d2074f9040ee7ceecb3e4ce15e17fa
AndrewFernando <72968022@continental.edu.pe>
2023-04-12 17:35:31 -0500

    v 5.3 Fernando

--- Operaciones.py
+++ Operaciones.py
@@ -1,7 +1,7 @@
-def suma(a,b):
-    return a+b
 def resta(a,b):
     return a-b
+def suma(a,b):
+    return a+b
 def multiplicacion(a,b):
     return a*b
 def division(a,b):
@@ -25,25 +25,25 @@ while True:
             b = float(input("Ingrese el segundo número: "))
             resultado = suma(a, b)
             print("Resultado: ", resultado)
-        if (opcion == 2):
+        elif (opcion == 2):
             a = float(input("Ingrese el primer número: "))
             b = float(input("Ingrese el segundo número: "))
             resultado = resta(a, b)
             print("Resultado: ", resultado)
-        if (opcion == 3):
+        elif (opcion == 3):
             a = float(input("Ingrese el primer número: "))
             b = float(input("Ingrese el segundo número: "))
             resultado = multiplicacion(a, b)
             print("Resultado: ", resultado)
-        if (opcion == 4):
+        elif (opcion == 4):
             a = float(input("Ingrese el primer número: "))
             b = float(input("Ingrese el segundo número: "))
             resultado = division(a, b)
             print("Resultado: ", resultado)
-        if (opcion == 5):
+        elif (opcion == 5):
             print("Gracias por usar la aplicación")
             break
     except ValueError as VE:
-        print("\n !!!!!Ingrese Dato Válido \n")
+        print("\n Error, Ingrese Dato Válido \n")
     except Exception as e:
-        print("Error",e)
+        print("Error General",e)
