c5937d5e9e63d2291c3d2543a2de4248cd8b3942
JoseVilca <70683227@continental.edu.pe>
2023-04-12 22:11:20 -0500

    Se agrego comentarios _Jose

--- Operaciones.py
+++ Operaciones.py
@@ -17,33 +17,34 @@ while True:
         print("4. Division")
         print("5. Salir")
         opcion = int(input("Ingrese una opción válida: "))
-        while(opcion < 1 or opcion > 5):
+        while(opcion < 1 or opcion > 5):        #   Validamos el dato que se va ingresar
+                                                #   Solo acepta numero del 1 al 4   
             print("Dato Inválido, vuelve a ingresar la opción")
             opcion = int(input("Ingrese una opción válida: "))
-        if (opcion == 1):
+        if (opcion == 1):       #La opcion 1 Suma
             a = float(input("Ingrese el primer número: "))
             b = float(input("Ingrese el segundo número: "))
             resultado = suma(a, b)
             print("Resultado: ", resultado)
-        if (opcion == 2):
+        if (opcion == 2):       #La opcion 2 Resta
             a = float(input("Ingrese el primer número: "))
             b = float(input("Ingrese el segundo número: "))
             resultado = resta(a, b)
             print("Resultado: ", resultado)
-        if (opcion == 3):
+        if (opcion == 3):       #La opcion 3 Multiplicacion
             a = float(input("Ingrese el primer número: "))
             b = float(input("Ingrese el segundo número: "))
             resultado = multiplicacion(a, b)
             print("Resultado: ", resultado)
-        if (opcion == 4):
+        if (opcion == 4):       #La opcion 4 Division
             a = float(input("Ingrese el primer número: "))
             b = float(input("Ingrese el segundo número: "))
             resultado = division(a, b)
             print("Resultado: ", resultado)
-        if (opcion == 5):
+        if (opcion == 5):       #La opcion 5 SALIR
             print("Gracias por usar la aplicación")
             break
-    except ValueError as VE:
+    except ValueError as VE:        #   Cual valor erroneo captura el error y lo ignora
         print("\n !!!!!Ingrese Dato Válido \n")
-    except Exception as e:
+    except Exception as e:          #   Cual valor erroneo captura el error y lo ignora
         print("Error",e)
